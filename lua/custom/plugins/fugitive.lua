4e957d38d51b0eb7b72c3648fc6a4e9abb8cdee5
Cramzzzberry <janroebantuan@gmail.com>
2024-09-20 05:21:35 +0800

    Added Git Status keybind

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,3 +1,6 @@
 return {
-	'https://github.com/tpope/vim-fugitive.git'
+	'https://github.com/tpope/vim-fugitive.git',
+	config = function()
+		vim.keymap.set('n', '<leader>g', '<cmd>vertical Git<CR>', { desc = '[G]it status' })
+	end
 }
